0b18bcfd30180d0605a51380639c9d82ceb30224
illiabutsenko <ilya1990.0826@gmail.com>
2024-03-05 19:52:13 +0100

    My Express.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my Express App!");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact us at: contact@example.com");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
