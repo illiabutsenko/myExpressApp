0fc7124a1ab239e9d566de437c74b2c7d13ea236
illiabutsenko <ilya1990.0826@gmail.com>
2024-03-25 18:07:20 +0100

    Add Rest API

--- app.js
+++ app.js
@@ -1,16 +1,66 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to my Express App!");
+app.use(express.json());
+
+let users = {};
+
+app.get("/users", (req, res) => {
+  res.status(200).json({ users });
 });
 
-app.get("/contact", (req, res) => {
-  res.send("Contact us at: contact@example.com");
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users[id];
+
+  if (!user) {
+    res.status(404).json({ error: "Користувача не існує" });
+    return;
+  }
+
+  res.status(200).json({ user: users[id] });
+});
+
+app.post("/users", (req, res) => {
+  const { id, name, email } = req.body;
+
+  if (users[id]) {
+    res.status(409).json({ error: "Користувача вже створено" });
+    return;
+  }
+
+  users[id] = { name, email };
+  res.status(201).json({ user: users[id] });
+});
+
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body;
+  const user = users[id];
+
+  if (!user) {
+    res.status(404).json({ error: "Користувача не існує" });
+    return;
+  }
+  users[id] = { name, email };
+  res.status(200).json({ user: users[id] });
+});
+
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users[id];
+
+  if (!user) {
+    res.status(404).json({ error: "Користувача не існує" });
+    return;
+  }
+
+  delete users[id];
+  res.status(200).json({ message: "User deleted successfully " });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
